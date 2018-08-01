a495b7eb03c6bc15a9733f266d20e0a8bfe84190
nigel-hall-codes <nigel682@gmail.com>
2018-08-01 20:58:40 +0000

    updated 1sec pricing

--- models.py
+++ models.py
@@ -1,22 +1,22 @@
 import pymysql
 
 cnx = pymysql.connect(
-    host='198.199.93.158',
+    host='127.0.0.1',
     port=3306,
-    user='root',
+    user='nhall',
     password='shutupPb0y',
-    database="CRYPTO"
+    database="TCN"
 )
 
 cursor = cnx.cursor()
 
 def store_data(timestamp, price, symbol):
 
-    select_database = "USE CRYPTO;"
+    select_database = "USE TCN;"
     insert_price = """
-        INSERT INTO binance_pricing (symbol, price, timestamp)
-        VALUES ("{}", {}, {});
-    """.format(symbol, price, timestamp)
+        INSERT INTO binance_pricing_1sec (timestamp, price, symbol)
+        VALUES ({}, {}, "{}");
+    """.format(timestamp, price, symbol)
     cursor.execute(select_database)
     cursor.execute(insert_price)
     cnx.commit()
---
+++ scraper1secBinance.py
@@ -0,0 +1,34 @@
+from binance.client import Client
+import datetime
+import time
+import models
+
+
+
+class Scraper:
+    def __init__(self):
+        self.api_key = '7tDBYdWpcmbHLSmJJKVd2aCVbzyGwdQ3hDgSfw5MrrXSpjAoufMW5Tkk02W1JiPv'
+        self.api_secret = '98BCmuZh6yFLJUYtJ7WtZtCqiMeNTuauxKfzrRQQ82RNnG01HJdxmuXzxZ5SSnyU'
+        self.client = Client(self.api_key, self.api_secret)
+
+    def run(self):
+        while True:
+            try:
+                tickers = self.client.get_all_tickers()
+                server_time = self.client.get_server_time()['serverTime']
+
+                for t in tickers:
+                    if t['symbol'] in ["ETHUSDT", "BTCUSDT"]:
+
+                        models.store_data(int(server_time), float(t['price']), t['symbol'])
+                time.sleep(1)
+            except Exception as e:
+                print(e)
+    def tickers(self):
+        return self.client.get_all_tickers()
+
+# s = Scraper()
+
+if __name__ == '__main__':
+    s = Scraper()
+    s.run()
